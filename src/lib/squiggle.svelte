df114cf72be46010df7bbb50ac7efe9be5c69dd3
aldottexe <aldottexe@gmail.com>
2025-12-10 19:01:38 -0500

    squiggle pretty good

--- src/lib/squiggle.svelte
+++ src/lib/squiggle.svelte
@@ -2,36 +2,160 @@
 	import type { Snippet } from 'svelte';
 	const { children }: { children: Snippet } = $props();
 
-	const wl = 20; // squiggle width
+	const wl = 15; // squiggle width
 	const amp = 20; // squiggle height
-	const duration = '1s';
-
+	const duration = '3s';
+	const slope = (2 * amp) / wl;
 	const period = wl * 2;
+	console.log('slope' + slope);
 
 	let clientWidth = $state(0);
 	let clientHeight = $state(0);
-	let wRounded = $derived(clientWidth - (clientWidth % period) + period);
-	let hRounded = $derived(clientHeight - (clientHeight % period) + period);
+	let wRounded = $derived(clientWidth - ((clientWidth - 2 * amp) % period));
+	let hRounded = $derived(clientHeight - ((clientHeight - 2 * amp) % period));
 
-	// let tb: string = $state();
-	// let lr: string = $state();
-	$effect(() => {
-		let horizontalBorder = 'M2,2 ';
-		for (let i = 0; i < clientWidth / wl + 1; i++) {
-			horizontalBorder += !(i % 2)
-				? `c${wl / 2},0,${wl / 2},${amp},${wl},${amp} `
-				: `c${wl / 2},0,${wl / 2},-${amp},${wl},-${amp} `;
+	interface corners_T {
+		tl: corner_T;
+		tr: corner_T;
+		bl: corner_T;
+		br: corner_T;
+	}
+	interface corner_T {
+		f1: frame_T;
+		f2: frame_T;
+		f3: frame_T;
+		f4: frame_T;
+	}
+	// p1 is absolute. p2-4 are relative to p1
+	interface frame_T {
+		p1: { x: number; y: number };
+		p2: { x: number; y: number };
+		p3: { x: number; y: number };
+		p4: { x: number; y: number };
+	}
+
+	const keyframeToString = (f) =>
+		`M${(f as frame_T).p1.x},${(f as frame_T).p1.y}` +
+		`c${(f as frame_T).p2.x},${(f as frame_T).p2.y},` +
+		`${(f as frame_T).p3.x},${(f as frame_T).p3.y},` +
+		`${(f as frame_T).p4.x},${(f as frame_T).p4.y};`;
+
+	const corners: corners_T = $derived({
+		tl: {
+			f1: {
+				p1: { x: amp - 2, y: amp },
+				p2: { x: 0, y: -2 },
+				p3: { x: 0, y: -2 },
+				p4: { x: 2, y: -2 }
+			},
+			f2: {
+				p1: { x: amp / 2, y: amp },
+				p2: { x: (amp * slope) / 4, y: -amp / 4 },
+				p3: { x: amp / 4, y: (-amp * slope) / 4 - amp / 2 },
+				p4: { x: amp / 2, y: -amp / 2 }
+			}
+			f3: {
+				p1: { x: 2, y: amp },
+				p2: { x: 0, y: -amp / 2 },
+				p3: { x: amp / 2, y: 2 - amp },
+				p4: { x: amp - 2, y: 2 - amp }
+			},
+			f4: {
+				p1: { x: amp / 2, y: amp },
+				p2: { x: -(amp * slope) / 4, y: -amp / 4 },
+				p3: { x: amp / 4, y: (amp * slope) / 4 - amp / 2 },
+				p4: { x: amp / 2, y: -amp / 2 }
+			}
+		},
+		tr: {
+			f1: {
+				p1: { x: wRounded - amp, y: amp - 2 },
+				p2: { x: amp / 2, y: 0 },
+				p3: { x: amp - 2, y: -amp / 2 },
+				p4: { x: amp - 2, y: 2 }
+			},
+			f2: {
+				p1: { x: wRounded - amp, y: amp / 2 },
+				p2: { x: amp / 4, y: (slope * amp) / 4 },
+				p3: { x: amp / 2 - (slope * amp) / 4, y: amp / 4 },
+				p4: { x: amp / 2, y: amp / 2 }
+			},
+			f3: {
+				p1: { x: wRounded - amp, y: 2 },
+				p2: { x: amp / 2, y: 0 },
+				p3: { x: 2, y: amp / 2 },
+				p4: { x: 2, y: amp - 2 }
+			},
+			f4: {
+				// mid left
+				p1: { x: wRounded - amp, y: amp / 2 },
+				p2: { x: amp / 4, y: -(slope * amp) / 4 },
+				p3: { x: amp / 2 + (slope * amp) / 4, y: -amp / 4 },
+				p4: { x: amp / 2, y: amp / 2 }
+			}
+		},
+		bl: {
+			f1: {
+				p1: { x: amp - 2, y: hRounded - amp },
+				p2: { x: 0, y: amp / 2 },
+				p3: { x: -amp / 2, y: amp - 2 },
+				p4: { x: 2, y: amp - 2 }
+			},
+			f2: {
+				p1: { x: amp / 2, y: hRounded - amp },
+				p2: { x: -(slope * amp) / 4, y: amp / 4 },
+				p3: { x: amp / 4, y: amp / 2 + (slope * amp) / 4 },
+				p4: { x: amp / 2, y: amp / 2 }
+			},
+			f3: {
+				p1: { x: 2, y: hRounded - amp },
+				p2: { x: 0, y: amp / 2 },
+				p3: { x: amp / 2, y: 2 },
+				p4: { x: amp - 2, y: 2 }
+			},
+			f4: {
+				p1: { x: amp / 2, y: hRounded - amp },
+				p2: { x: (slope * amp) / 4, y: amp / 4 },
+				p3: { x: amp / 4, y: amp / 2 - (slope * amp) / 4 },
+				p4: { x: amp / 2, y: amp / 2 }
+			}
+		},
+		br: {
+			f1: {
+				p1: { x: wRounded - amp, y: hRounded - 2 },
+				p2: { x: amp / 2, y: 0 },
+				p3: { x: amp - 2, y: -amp / 2 },
+				p4: { x: amp - 2, y: 2 - amp }
+			},
+			f2: {
+				//med left
+				p1: { x: wRounded - amp, y: hRounded - amp / 2 },
+				p2: { x: amp / 4, y: -(slope * amp) / 4 },
+				p3: { x: amp / 2 + (slope * amp) / 4, y: -amp / 4 },
+				p4: { x: amp / 2, y: -amp / 2 }
+			},
+			f3: {
+				p1: { x: wRounded - amp, y: hRounded - amp + 2 },
+				p2: { x: 2, y: 0 },
+				p3: { x: 2, y: 0 },
+				p4: { x: 2, y: -2 }
+			},
+			f4: {
+				p1: { x: wRounded - amp, y: hRounded - amp / 2 },
+				p2: { x: amp / 4, y: (slope * amp) / 4 },
+				p3: { x: amp / 2 - (slope * amp) / 4, y: -amp / 4 },
+				p4: { x: amp / 2, y: -amp / 2 }
+			}
 		}
-		// tb = horizontalBorder;
 	});
 </script>
 
 <div class="relative w-fit p-5" bind:clientWidth bind:clientHeight>
 	{@render children()}
-	<svg viewBox="0 0 {wRounded} {hRounded}" class="absolute inset-0 z-[-1]">
+	<svg viewBox="0 0 {wRounded} {hRounded}" class="absolute inset-0 z-[999]">
 		<defs>
 			<!-- top -->
-			<pattern id="tpattern" width={2 * wl} height={amp} patternUnits="userSpaceOnUse">
+			<pattern id="tpattern" width={2 * wl} height={amp} patternUnits="userSpaceOnUse" x={amp - wl}>
 				<path
 					class="fill-transparent stroke-main-orange stroke-2"
 					stroke-width="2"
@@ -52,7 +176,7 @@
 				</path>
 			</pattern>
 			<!-- bottom -->
-			<pattern id="bpattern" width={2 * wl} height={amp} patternUnits="userSpaceOnUse">
+			<pattern id="bpattern" width={2 * wl} height={amp} patternUnits="userSpaceOnUse" x={amp - wl}>
 				<path
 					class="fill-transparent stroke-main-orange stroke-2"
 					stroke-width="2"
@@ -73,7 +197,7 @@
 				</path>
 			</pattern>
 			<!-- left -->
-			<pattern id="lpattern" width={amp} height={2 * wl} patternUnits="userSpaceOnUse">
+			<pattern id="lpattern" width={amp} height={2 * wl} patternUnits="userSpaceOnUse" y={amp - wl}>
 				<path
 					class="fill-transparent stroke-main-orange stroke-2"
 					stroke-width="2"
@@ -95,7 +219,14 @@
 				</path>
 			</pattern>
 			<!-- right -->
-			<pattern id="rpattern" width={amp} height={2 * wl} patternUnits="userSpaceOnUse">
+			<pattern
+				id="rpattern"
+				width={amp}
+				height={2 * wl}
+				patternUnits="userSpaceOnUse"
+				y={amp - wl}
+				x={wRounded}
+			>
 				<path
 					class="fill-transparent stroke-main-orange stroke-2"
 					stroke-width="2"
@@ -117,6 +248,7 @@
 				</path>
 			</pattern>
 		</defs>
+
 		<!-- top -->
 		<rect x={amp} width={wRounded - 2 * amp} height={amp} fill="url(#tpattern)" />
 		<!-- bottom -->
@@ -137,24 +269,28 @@
 			width={amp}
 			fill="url(#rpattern)"
 		/>
-		<!-- tl-->
-		<path class="fill-transparent stroke-main-orange stroke-2" stroke-width="2">
-			<animate
-				attributeName="d"
-				dur={duration}
-				repeatDur="indefinite"
-				values="
-					M{amp - 2},{amp}
-					C{amp - 2},{amp - 2},{amp - 2},{amp - 2},{amp},{amp - 2};
-					M2,{amp}
-					C2,{amp / 2},{amp / 2} ,2,{amp},2;
-					M{amp - 2},{amp}
-					C{amp - 2},{amp - 2},{amp - 2},{amp - 2},{amp},{amp - 2};
-						"
-				calcMode="spline"
-				keySplines="0.5 0 0.5 1; 0.5 0 0.5 1"
-				keyTimes="0;0.5;1"
-			/>
-		</path>
+
+		<!-- corners -->
+		{#each Object.values(corners) as c, i (i)}
+			<path
+				class="fill-transparent stroke-main-orange stroke-2"
+				stroke-width="2"
+				stroke-linecap="round"
+			>
+				<animate
+					attributeName="d"
+					dur={duration}
+					repeatDur="indefinite"
+					values="
+               {Object.values(c).map(keyframeToString).join('\n') + keyframeToString(c.f1)}"
+					calcMode="spline"
+					keySplines="0.5 0 0.75 0.5; 
+                        0.25 0.5 0.5 1;
+                        0.5 0 0.75 0.5; 
+                        0.25 0.5 0.5 1;"
+					keyTimes="0;0.25;0.5;0.75;1"
+				/>
+			</path>
+		{/each}
 	</svg>
 </div>
