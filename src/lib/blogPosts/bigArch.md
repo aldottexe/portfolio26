a60ca3e3b8a915f718a6b89653d5d1d0eac74b8b
aldottexe <aldottexe@gmail.com>
2026-06-27 23:40:07 -0400

    basic templating

---
+++ src/lib/blogPosts/bigArch.md
@@ -0,0 +1,10 @@
+---
+emoji: 🍔
+featureImage: /200kmi/200kmiHero.gif
+featureVid: /200kmi/200kmivid@720.mov
+title: Big Arch Review
+desc: A RAW UNFILTERED review of McDonald's new burger💪
+tags: etc
+---
+
+Really good size. The paddies were actually thick, unlike the shameful size of the Big Mac’s or the mc double’s. The Arch Sauce was tasty. not too different than Big Mac sauce, but perhaps a little more savory. The bun was light like most McDonald’s burgers—which I usually don’t mind—but it struggled to stay in one piece with everything that was on the burger. The highlight of the brew is definitely the fried onions. They were crunchy almost the consistency of McDonald fries. 
---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import type { PageProps } from '../$types';
+	// import Backdrop from '$lib/Backdrop.svelte';
+	// import Border from '$lib/Border.svelte';
+
+	const { data }: PageProps = $props();
+	const Post = data.component;
+	const meta = data.metadata;
+</script>
+
+<div
+	class="prose
+      min-h-screen
+      max-w-full
+      px-10
+      pt-20
+      prose-headings:mx-auto
+      prose-headings:max-w-150
+      prose-headings:text-main-white
+      prose-p:mx-auto
+      prose-p:max-w-150
+      prose-p:text-main-white
+      prose-a:text-main-green
+      prose-a:no-underline
+      prose-a:hover:text-main-blue
+      prose-img:mx-auto
+      prose-img:max-h-[calc(100vh-80px)]
+      prose-p:[&:has(>img:first-child)]:max-w-170"
+>
+	<Post />
+</div>
