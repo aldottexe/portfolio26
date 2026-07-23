b1b6330294ad975dc1d4fa6e4a186ae6ca5d0978
Alex Lutz <100101459+aldottexe@users.noreply.github.com>
2026-07-23 23:58:33 +0000

    mask moves with scroll. change points to icon

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import ProjectList from '$lib/ProjectList.svelte';
-
 	import type { Action } from 'svelte/action';
 	import { fade } from 'svelte/transition';
 	import { onMount } from 'svelte';
 	import { Spring } from 'svelte/motion';
+	import type { UIEventHandler } from 'svelte/elements';
 	const { data } = $props();
+
+	const patternDistance = 150;
 
 	let setSize: (size: { width: number; height: number }) => void;
 	const size = new Promise<{ width: number; height: number }>((resolve) => {
@@ -21,42 +23,51 @@
 		x: number;
 		y: number;
 	}
-	let setMaskSize: (size: maskSize_T) => void;
-	const maskSize: Promise<maskSize_T> = new Promise((resolve) => {
-		setMaskSize = resolve;
-	});
+
+	let maskSize: maskSize_T | undefined = $state();
+
+	let scroll = $state(0);
+
+	function updateMask(node: HTMLDivElement) {
+		const r = node.getBoundingClientRect()
+		maskSize = {
+			w: r.width,
+			h: r.height,
+			x: r.x,
+			y: r.y
+		};
+	}
+	const updateMaskFromEvent: UIEventHandler<HTMLDivElement> = (e) => {
+		updateMask(e.currentTarget)
+	}
+
 
 	let mask: Action<HTMLDivElement> = (node) => {
-		setMaskSize({
-			w: node.offsetWidth,
-			h: node.offsetHeight,
-			x: node.offsetLeft,
-			y: node.offsetTop
-		});
+		updateMask(node)
 	};
 
 	let mouse = new Spring({ x: 0, y: 0 });
-	let fieldPoint: Action<HTMLSpanElement, maskSize_T> = (node, mask) => {
-		if (
-			node.offsetLeft < mask.x ||
-			node.offsetLeft > mask.x + mask.w ||
-			node.offsetTop < mask.y ||
-			node.offsetTop > mask.y + mask.h
-		) {
-			node.style.transition = 'transform 1200ms cubic-bezier(.2,.8,.2,1)';
-			$effect(() => {
-				const atan = Math.atan2(
-					mouse.current.y - node.offsetTop - node.offsetHeight / 2,
-					mouse.current.x - node.offsetLeft - node.offsetWidth / 2
-				);
-				const r_up =
-					atan > Math.PI / 2 ? atan - Math.PI : atan < -Math.PI / 2 ? atan + Math.PI : atan;
-				const deg = (r_up * 180) / Math.PI;
-				node.style.transform = `rotate(${deg}deg)`;
-			});
-		} else {
-			node.style.opacity = '0';
-		}
+	let fieldPoint: Action<HTMLSpanElement> = (node) => {
+		$effect(()=>{
+			node.style.opacity = maskSize && (
+			node.offsetLeft + node.offsetWidth < maskSize.x ||
+			node.offsetLeft > maskSize.x + maskSize.w ||
+			node.offsetTop + node.offsetHeight + node.offsetWidth / 2 + 10 < maskSize.y - scroll ||
+			node.offsetTop > maskSize.y + maskSize.h - scroll
+		) ? '100' : '0';
+		});
+		
+		node.style.transition = 'transform 1200ms cubic-bezier(.2,.8,.2,1), opacity 200ms ease-in-out';
+		$effect(() => {
+			const atan = Math.atan2(
+				mouse.current.y - node.offsetTop - node.offsetHeight / 2,
+				mouse.current.x - node.offsetLeft - node.offsetWidth / 2
+			);
+			const r_up =
+				atan > Math.PI / 2 ? atan - Math.PI : atan < -Math.PI / 2 ? atan + Math.PI : atan;
+			const deg = (r_up * 180) / Math.PI;
+			node.style.transform = `rotate(${deg}deg)`;
+		});
 	};
 </script>
 
@@ -64,52 +75,52 @@
 	on:mousemove={(e) => {
 		mouse.set({ x: e.clientX, y: e.clientY });
 	}}
+	bind:scrollY={scroll}
 />
+
+{#snippet loader()}
+<span
+		class="fixed top-0 left-0 z-3 h-full w-full bg-main-black flex items-center justify-center"
+		transition:fade|global={{ duration: 300 }}>ummmmm...</span
+	>
+{/snippet}
 
 <!-- background -->
 {#await size}}
-	<span
-		class="fixed top-0 left-0 z-3 h-full w-full bg-main-black"
-		transition:fade|global={{ duration: 300 }}>ummmmm...</span
-	>
+	{@render loader()}
 {:then s}
-	{#await maskSize}}
-		<span
-			class="fixed top-0 left-0 z-3 h-full w-full bg-main-black"
-			transition:fade|global={{ duration: 300 }}>ummmmm...</span
-		>
-	{:then m}
-		{#if mouse.current.x > 0}
-			<div class="absolute top-0 left-0 z-0 flex h-screen w-full flex-col justify-around">
-				{#each new Array(Math.floor(s.height / 100)) as _, i (i)}
+		{#if mouse.current.x > 0 && maskSize}
+			<div class="fixed top-0 left-0 z-0 flex h-screen w-full flex-col justify-around">
+				{#each new Array(Math.floor(s.height / patternDistance)) as _, i (i)}
 					<div class="flex justify-around">
-						{#each new Array(Math.floor(s.width / 100)) as _, j (j)}
+						{#each new Array(Math.floor(s.width / patternDistance)) as _, j (j)}
 							<span
-								class="rounded-xs bg-main-white px-1 text-xs text-main-black"
+								class="rounded-xs px-1 text-xs text-main-black"
 								transition:fade|global={{
 									duration: 300,
-									delay: Math.hypot(Math.abs(j - 5), Math.abs(i - 5)) * 300
+									delay: Math.hypot(Math.abs(j - 5), Math.abs(i - 5)) * 150
 								}}
-								use:fieldPoint={m}
+								use:fieldPoint
 							>
-								Alex's Blog
+									<svg class="fill-main-green" width="50" height="34">
+										<use href="/logo small.svg#shape" class="scale-[0.4]" />
+									</svg>
 							</span>
 						{/each}
 					</div>
 				{/each}
 			</div>
 		{/if}
-	{/await}
-
-	<div class="relative z-10 mx-auto box-content max-w-200 px-10">
-		<div class="flex h-screen items-center justify-center">
-			<div class="padding-sm rounded-md bg-main-black" use:mask>
+{/await}
+<div class="pt-[50vh]">
+	<div class="relative z-10 mx-auto box-content max-w-200 px-10" use:mask>
+			<div class="py-5 px-2" 
+			onresize={updateMaskFromEvent}
+			>
 				<h1 class="mb-1">Alex's Blog</h1>
-				<p class="mb-5">Here you can find all of my non-work related ramblings.</p>
+				<p class="mb-5">Hi I'm alex, and this is my blog. Here you can find all kinds of ramblings. From experiments in tech, to life updates, to opinion pieces. Whatever you're looking for, I hope you find something you like.</p>
 			</div>
-		</div>
 
 		<ProjectList projects={data.projects} />
 	</div>
-{/await}
-
+</div>
