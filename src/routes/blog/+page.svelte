1c0b7301cd73d29908a0eb4e99391ea55a1113c3
aldottexe <aldottexe@gmail.com>
2026-07-23 11:25:19 -0400

    add images

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
 	import ProjectList from '$lib/ProjectList.svelte';
-    
+
 	import type { Action } from 'svelte/action';
-	import {fade} from 'svelte/transition';
+	import { fade } from 'svelte/transition';
 	import { onMount } from 'svelte';
 	import { Spring } from 'svelte/motion';
 	const { data } = $props();
 
-	let setSize: (size: {width: number, height: number}) => void;
-	const size = new Promise<{width: number, height: number}>((resolve) => {
+	let setSize: (size: { width: number; height: number }) => void;
+	const size = new Promise<{ width: number; height: number }>((resolve) => {
 		setSize = resolve;
 	});
 	onMount(() => {
@@ -27,75 +27,89 @@
 	});
 
 	let mask: Action<HTMLDivElement> = (node) => {
-		setMaskSize({ w: node.offsetWidth, h: node.offsetHeight, x: node.offsetLeft, y: node.offsetTop });
-	}
+		setMaskSize({
+			w: node.offsetWidth,
+			h: node.offsetHeight,
+			x: node.offsetLeft,
+			y: node.offsetTop
+		});
+	};
 
 	let mouse = new Spring({ x: 0, y: 0 });
 	let fieldPoint: Action<HTMLSpanElement, maskSize_T> = (node, mask) => {
 		if (
-			node.offsetLeft < mask.x 
-			|| node.offsetLeft > mask.x + mask.w 
-			|| node.offsetTop < mask.y 
-			|| node.offsetTop > mask.y + mask.h
+			node.offsetLeft < mask.x ||
+			node.offsetLeft > mask.x + mask.w ||
+			node.offsetTop < mask.y ||
+			node.offsetTop > mask.y + mask.h
 		) {
-node.style.transition = 'transform 1200ms cubic-bezier(.2,.8,.2,1)';
-		$effect(() => {
-			const atan = Math.atan2(mouse.current.y - node.offsetTop - node.offsetHeight / 2, mouse.current.x - node.offsetLeft - node.offsetWidth / 2);
-			const r_up = atan > Math.PI / 2 ? atan - Math.PI : atan < -Math.PI / 2 ? atan + Math.PI : atan;
-			const deg = r_up * 180 / Math.PI;
-			node.style.transform = `rotate(${deg}deg)`;
-		});
+			node.style.transition = 'transform 1200ms cubic-bezier(.2,.8,.2,1)';
+			$effect(() => {
+				const atan = Math.atan2(
+					mouse.current.y - node.offsetTop - node.offsetHeight / 2,
+					mouse.current.x - node.offsetLeft - node.offsetWidth / 2
+				);
+				const r_up =
+					atan > Math.PI / 2 ? atan - Math.PI : atan < -Math.PI / 2 ? atan + Math.PI : atan;
+				const deg = (r_up * 180) / Math.PI;
+				node.style.transform = `rotate(${deg}deg)`;
+			});
 		} else {
 			node.style.opacity = '0';
 		}
-		
-	}
-
+	};
 </script>
-<svelte:window on:mousemove={(e) => {
+
+<svelte:window
+	on:mousemove={(e) => {
 		mouse.set({ x: e.clientX, y: e.clientY });
 	}}
 />
 
 <!-- background -->
 {#await size}}
-	<span class="fixed top-0 left-0 w-full h-full bg-main-black z-3" transition:fade|global={{ duration: 300 }}>ummmmm...</span>
-{:then s} 
-	{#await maskSize}}
-		<span class="fixed top-0 left-0 w-full h-full bg-main-black z-3" transition:fade|global={{ duration: 300 }}>ummmmm...</span>
-	{:then m}
-	{#if mouse.current.x > 0}
-		<div class="flex justify-around h-screen flex-col absolute top-0 left-0 w-full z-0">
-			{#each new Array(Math.floor(s.height / 100)) as _, i}
-		<div class="flex justify-around">
-		{#each new Array(Math.floor(s.width / 100)) as _, j}
-			<span 
-				class="text-xs bg-main-white text-main-black rounded-xs px-1" 
-				transition:fade|global={{ duration: 300, delay: Math.hypot(Math.abs(j- 5), Math.abs(i- 5)) * 300 }} 
-				use:fieldPoint={m}
-			>
-				Alex's Blog
-			</span>
-		{/each}
-		</div>	
-	{/each}
-
-</div>
-{/if}
-{/await}
-
-
-<div class="mx-auto box-content max-w-200 px-10 z-10 relative">
-<div class="h-screen flex justify-center items-center">	
-	<div class="bg-main-black rounded-md padding-lg rounded-lg px-5 py-2"
-	use:mask
+	<span
+		class="fixed top-0 left-0 z-3 h-full w-full bg-main-black"
+		transition:fade|global={{ duration: 300 }}>ummmmm...</span
 	>
-	<h1 class="mb-1">Alex's Blog</h1>
-		<p class="mb-5">Here you can find all of my non-work related ramblings.</p>
-	</div>	
+{:then s}
+	{#await maskSize}}
+		<span
+			class="fixed top-0 left-0 z-3 h-full w-full bg-main-black"
+			transition:fade|global={{ duration: 300 }}>ummmmm...</span
+		>
+	{:then m}
+		{#if mouse.current.x > 0}
+			<div class="absolute top-0 left-0 z-0 flex h-screen w-full flex-col justify-around">
+				{#each new Array(Math.floor(s.height / 100)) as _, i (i)}
+					<div class="flex justify-around">
+						{#each new Array(Math.floor(s.width / 100)) as _, j (j)}
+							<span
+								class="rounded-xs bg-main-white px-1 text-xs text-main-black"
+								transition:fade|global={{
+									duration: 300,
+									delay: Math.hypot(Math.abs(j - 5), Math.abs(i - 5)) * 300
+								}}
+								use:fieldPoint={m}
+							>
+								Alex's Blog
+							</span>
+						{/each}
+					</div>
+				{/each}
+			</div>
+		{/if}
+	{/await}
 
-</div>
+	<div class="relative z-10 mx-auto box-content max-w-200 px-10">
+		<div class="flex h-screen items-center justify-center">
+			<div class="padding-sm rounded-md bg-main-black" use:mask>
+				<h1 class="mb-1">Alex's Blog</h1>
+				<p class="mb-5">Here you can find all of my non-work related ramblings.</p>
+			</div>
+		</div>
 
-	<ProjectList projects={data.projects} />
-</div>
+		<ProjectList projects={data.projects} />
+	</div>
 {/await}
+
