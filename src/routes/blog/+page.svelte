7921543174f18182f0640d7969085c0c1b9921e2
Alex Lutz <100101459+aldottexe@users.noreply.github.com>
2026-07-25 17:42:14 +0000

    smoother background animations

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import ProjectList from '$lib/ProjectList.svelte';
 	import type { Action } from 'svelte/action';
-	import { fade } from 'svelte/transition';
+	import { scale } from 'svelte/transition';
 	import { onMount } from 'svelte';
 	import { Spring } from 'svelte/motion';
 	import type { DOMAttributes, UIEventHandler } from 'svelte/elements';
 	const { data } = $props();
 
-	const patternDistance = 150;
+	const patternDistance = 120;
 
 	let windowWidth = $state(0);
 	let windowHeight = $state(0);
@@ -48,27 +48,35 @@
 	let fieldPoint: Action<HTMLSpanElement> = (node) => {
 		$effect(() => {
 			const rect = node.getBoundingClientRect();
-			node.style.opacity =
+			node.style.scale =
 				maskSize && (
 					rect.right < maskSize.x ||
 					rect.left > maskSize.right ||
 					rect.bottom < maskSize.y ||
 					rect.top > maskSize.bottom
 				)
-					? '100'
+					? '1'
 					: '0'
 		});
 		
-		node.style.transition = 'transform 1200ms cubic-bezier(.2,.8,.2,1), opacity 300ms ease-in-out';
+		node.style.transition = `transform 1200ms cubic-bezier(.2,.8,.2,1), scale 1200ms cubic-bezier(.2,.8,.2,1) ${Math.floor(Math.random() * 200)}ms`;
+		let lastDeg = 0;
 		$effect(() => {
 			const atan = Math.atan2(
 				mouse.current.y - node.offsetTop - node.offsetHeight / 2,
 				mouse.current.x - node.offsetLeft - node.offsetWidth / 2
 			);
-			const r_up =
-				atan > Math.PI / 2 ? atan - Math.PI : atan < -Math.PI / 2 ? atan + Math.PI : atan;
-			const deg = (r_up * 180) / Math.PI;
-			node.style.transform = `rotate(${deg}deg)`;
+			// const r_up =
+			// 	atan > Math.PI / 2 ? atan - Math.PI : atan < -Math.PI / 2 ? atan + Math.PI : atan;
+			const deg = (atan * 180) / Math.PI;
+			if (lastDeg % 360 - deg > 180) lastDeg += 360;
+			if (lastDeg % 360 - deg < -180) lastDeg -= 360;
+
+			const newdeg = lastDeg - lastDeg % 360 + deg;
+
+			lastDeg = newdeg;
+			const scale = mouse.current.x > node.offsetLeft + node.offsetWidth / 2 ? 0 : 180;
+			node.style.transform = `rotate(${newdeg}deg) rotateY(${scale}deg) rotateX(${scale}deg)`;
 		});
 	};
 </script>
@@ -85,24 +93,25 @@
 <!-- background -->
 
 {#if mouse.current.x > 0 && maskSize && winSizeSynced}
-	<div class="fixed top-0 left-0 z-0 flex h-screen w-full flex-col justify-between box-border px-5 py-4 md:px-9 md:py-8"
+	<div class="fixed top-0 left-0 z-0 flex h-screen w-full flex-col justify-between box-border px-5 py-20 md:px-30 md:py-20"
 	>
 		{#each new Array(Math.floor(windowHeight / patternDistance)) as _, i (i)}
 			<div class="flex justify-between">
 				{#each new Array(Math.floor(windowWidth / patternDistance)) as _, j (j)}
 					<span
 						class="rounded-xs px-1 text-xs text-main-black"
-						in:fade|global={{
-							duration: 300,
-							delay: Math.hypot(
-								Math.abs(j), 
-								Math.abs(i)
-							) * 150
-						}}
 						use:fieldPoint
 					>
-							<svg class="fill-main-green" width="50" height="34">
-								<use href="/logo small.svg#shape" class="scale-[0.4]" />
+							<svg 
+								class="fill-main-green" width="34" height="24" 						
+								in:scale|global={{
+									duration: 300,
+									delay: Math.hypot(
+										Math.abs(j), 
+										Math.abs(i)
+									) * 150
+								}}>
+								<use href="/logo small.svg#shape" class="scale-[0.3]" />
 							</svg>
 					</span>
 				{/each}
@@ -112,8 +121,8 @@
 {/if}
 
 
-<div class="pt-[50vh]">
-	<div class="relative z-10 mx-auto box-content max-w-200 px-10 pb-3" use:mask>
+<div class="pt-[35vh]">
+	<div class="relative z-5 mx-auto box-content max-w-200 px-10 pb-3" use:mask>
 			<div class="py-5 px-2" 
 			onresize={updateMask}
 			>
