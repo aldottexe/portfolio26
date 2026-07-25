769cc14731e0cac9479a1367effcdc6ac79654df
Alex Lutz <100101459+aldottexe@users.noreply.github.com>
2026-07-25 16:32:32 +0000

    pattern respondes to page resize

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -4,60 +4,62 @@
 	import { fade } from 'svelte/transition';
 	import { onMount } from 'svelte';
 	import { Spring } from 'svelte/motion';
-	import type { UIEventHandler } from 'svelte/elements';
+	import type { DOMAttributes, UIEventHandler } from 'svelte/elements';
 	const { data } = $props();
 
 	const patternDistance = 150;
 
-	let setSize: (size: { width: number; height: number }) => void;
-	const size = new Promise<{ width: number; height: number }>((resolve) => {
-		setSize = resolve;
-	});
-	onMount(() => {
-		setSize({ width: window.innerWidth, height: window.innerHeight });
-	});
+	let windowWidth = $state(0);
+	let windowHeight = $state(0);
 
-	interface maskSize_T {
-		w: number;
-		h: number;
-		x: number;
-		y: number;
-	}
+	let maskSize: DOMRect | undefined = $state();
 
-	let maskSize: maskSize_T | undefined = $state();
+	const mouse = new Spring({ x: 0, y: 0 });
 
-	let scroll = $state(0);
-
-	function updateMask(node: HTMLDivElement) {
-		const r = node.getBoundingClientRect()
-		maskSize = {
-			w: r.width,
-			h: r.height,
-			x: r.x,
-			y: r.y
-		};
-	}
-	const updateMaskFromEvent: UIEventHandler<HTMLDivElement> = (e) => {
-		updateMask(e.currentTarget)
-	}
-
+	let updateMask: (() => void) | undefined = $state();
 
 	let mask: Action<HTMLDivElement> = (node) => {
-		updateMask(node)
+		updateMask = () => {
+			maskSize = node.getBoundingClientRect()
+		}
+		requestAnimationFrame(updateMask)
 	};
 
-	let mouse = new Spring({ x: 0, y: 0 });
+	let winSizeSynced = $state(false);
+	let winSizeTimeout: NodeJS.Timeout | null = null;
+	const resizeThrottler: UIEventHandler<Window> = (e) => {
+		if (winSizeTimeout) clearTimeout(winSizeTimeout);
+
+		winSizeSynced = false;
+		winSizeTimeout = setTimeout(() => {
+			const targetWindow = e.currentTarget as Window;
+			windowWidth = targetWindow.innerWidth;
+			windowHeight = targetWindow.innerHeight;
+			if (updateMask) updateMask();
+			winSizeSynced = true;
+		}, 300);
+	}
+	const win: Action<Window> = (node) => {
+		windowWidth = node.innerWidth;
+		windowHeight = node.innerHeight;
+		winSizeSynced = true;
+	}
+
 	let fieldPoint: Action<HTMLSpanElement> = (node) => {
-		$effect(()=>{
-			node.style.opacity = maskSize && (
-			node.offsetLeft + node.offsetWidth < maskSize.x ||
-			node.offsetLeft > maskSize.x + maskSize.w ||
-			node.offsetTop + node.offsetHeight + node.offsetWidth / 2 + 10 < maskSize.y - scroll ||
-			node.offsetTop > maskSize.y + maskSize.h - scroll
-		) ? '100' : '0';
+		$effect(() => {
+			const rect = node.getBoundingClientRect();
+			node.style.opacity =
+				maskSize && (
+					rect.right < maskSize.x ||
+					rect.left > maskSize.right ||
+					rect.bottom < maskSize.y ||
+					rect.top > maskSize.bottom
+				)
+					? '100'
+					: '0'
 		});
 		
-		node.style.transition = 'transform 1200ms cubic-bezier(.2,.8,.2,1), opacity 200ms ease-in-out';
+		node.style.transition = 'transform 1200ms cubic-bezier(.2,.8,.2,1), opacity 300ms ease-in-out';
 		$effect(() => {
 			const atan = Math.atan2(
 				mouse.current.y - node.offsetTop - node.offsetHeight / 2,
@@ -75,47 +77,45 @@
 	on:mousemove={(e) => {
 		mouse.set({ x: e.clientX, y: e.clientY });
 	}}
-	bind:scrollY={scroll}
+	on:scroll={updateMask}
+	on:resize={resizeThrottler}
+	use:win
 />
 
-{#snippet loader()}
-<span
-		class="fixed top-0 left-0 z-3 h-full w-full bg-main-black flex items-center justify-center"
-		transition:fade|global={{ duration: 300 }}>ummmmm...</span
-	>
-{/snippet}
-
 <!-- background -->
-{#await size}}
-	{@render loader()}
-{:then s}
-		{#if mouse.current.x > 0 && maskSize}
-			<div class="fixed top-0 left-0 z-0 flex h-screen w-full flex-col justify-around">
-				{#each new Array(Math.floor(s.height / patternDistance)) as _, i (i)}
-					<div class="flex justify-around">
-						{#each new Array(Math.floor(s.width / patternDistance)) as _, j (j)}
-							<span
-								class="rounded-xs px-1 text-xs text-main-black"
-								transition:fade|global={{
-									duration: 300,
-									delay: Math.hypot(Math.abs(j - 5), Math.abs(i - 5)) * 150
-								}}
-								use:fieldPoint
-							>
-									<svg class="fill-main-green" width="50" height="34">
-										<use href="/logo small.svg#shape" class="scale-[0.4]" />
-									</svg>
-							</span>
-						{/each}
-					</div>
+
+{#if mouse.current.x > 0 && maskSize && winSizeSynced}
+	<div class="fixed top-0 left-0 z-0 flex h-screen w-full flex-col justify-between box-border px-5 py-4 md:px-9 md:py-8"
+	>
+		{#each new Array(Math.floor(windowHeight / patternDistance)) as _, i (i)}
+			<div class="flex justify-between">
+				{#each new Array(Math.floor(windowWidth / patternDistance)) as _, j (j)}
+					<span
+						class="rounded-xs px-1 text-xs text-main-black"
+						in:fade|global={{
+							duration: 300,
+							delay: Math.hypot(
+								Math.abs(j), 
+								Math.abs(i)
+							) * 150
+						}}
+						use:fieldPoint
+					>
+							<svg class="fill-main-green" width="50" height="34">
+								<use href="/logo small.svg#shape" class="scale-[0.4]" />
+							</svg>
+					</span>
 				{/each}
 			</div>
-		{/if}
-{/await}
+		{/each}
+	</div>
+{/if}
+
+
 <div class="pt-[50vh]">
-	<div class="relative z-10 mx-auto box-content max-w-200 px-10" use:mask>
+	<div class="relative z-10 mx-auto box-content max-w-200 px-10 pb-3" use:mask>
 			<div class="py-5 px-2" 
-			onresize={updateMaskFromEvent}
+			onresize={updateMask}
 			>
 				<h1 class="mb-1">Alex's Blog</h1>
 				<p class="mb-5">Hi I'm alex, and this is my blog. Here you can find all kinds of ramblings. From experiments in tech, to life updates, to opinion pieces. Whatever you're looking for, I hope you find something you like.</p>
