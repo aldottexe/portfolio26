3040eda936ff9610b8f25fadaf1a659384213a64
Alex Lutz <100101459+aldottexe@users.noreply.github.com>
2026-07-26 17:46:40 +0000

    UNTESTED // replace css animation with frame animation

--- src/lib/PageTransition.svelte
+++ src/lib/PageTransition.svelte
@@ -1,23 +1,61 @@
 <script lang="ts">
 	import { onNavigate } from '$app/navigation';
+	import type { Action } from '@sveltejs/kit';
+	import { tick } from 'svelte';
+
+	const frames: Record<string, ()=>Promise<unknown>> = import.meta.glob('/static/transition/*')
+	const links = Object.keys(frames).map(k => k.replaceAll(/\/static/g, ""));
+	console.log(frames);
 
 	let transitioning = $state(false);
+	let frame = $state(0);
+
+	function animate(inc: boolean = true) {
+		function step(inc: boolean = true, res: () => void) {
+			if (inc) {
+				frame++;
+				if (frame < links.length - 1)
+					setTimeout(animate, 1000 / 3);
+				else
+					res();
+			} else {
+				frame--;
+				if (frame > 0)
+					setTimeout(animate, 1000 / 3);
+				else
+					res();
+			}	
+		}
+		return new Promise<void>((res) => step(inc, res));
+	}
+	
 
 	onNavigate((nav) => {
 		return new Promise(async (res) => {
 			transitioning = true;
-			await new Promise((res) => setTimeout(res, 300));
+			await animate(true);
 			res();
 			await nav.complete;
-			await new Promise((res) => setTimeout(res, 200));
+			await tick();
+			await animate(false);
 			transitioning = false;
 		});
 	});
+
 </script>
 
-<div
-	class={`fixed ${transitioning ? 'bottom-0 h-full ease-out' : 'top-0 h-0 ease-in'} z-50 w-full bg-main-orange transition-[height] duration-300`}
-></div>
+<!-- Preload all transition frames to avoid flickering -->
+<svelte:head>
+	{#each links as i}
+		<link rel="preload" href={i} as="image"/>
+	{/each}
+</svelte:head>
+
+<img
+	class={`fixed h-full object-cover z-50 w-full bg-main-orange transition-[height] duration-300`}
+	src={links[frame]}
+	alt="Page is Loading"
+/>
 
 <style lang="postcss">
 	@reference "tailwindcss";
